d7fdaf7880535bd8a42bde249dc88763036684f8
lucarnel1 <lucarnel1@gmail.com>
2025-03-28 14:11:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Fonction qui change le texte du message
 document.getElementById('changerMessage').addEventListener('click', function() {
   document.getElementById('message').innerText = "Le message a été changé !";
-  alert('Hello world!');
 });
