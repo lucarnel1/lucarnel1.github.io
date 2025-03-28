6645caa392edf23a7fc25a61895265e1b9a202fa
lucarnel1 <lucarnel1@gmail.com>
2025-03-28 08:45:45 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Fonction qui change le texte du message
+document.getElementById('changerMessage').addEventListener('click', function() {
+  document.getElementById('message').innerText = "Le message a été changé !";
+});
